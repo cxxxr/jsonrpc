0830078ce44a25cce180e6f6c230af2e33651307
Eitaro Fukamachi <e.arrows@gmail.com>
2017-02-20 14:57:41 +0900

    Add :connect-cb for websocket-transport.

--- transport/websocket.lisp
+++ transport/websocket.lisp
@@ -22,6 +22,9 @@
    (server :accessor websocket-transport-server
            :initarg :server
            :initform :hunchentoot)
+   (connect-cb :initarg :connect-cb
+               :type (or null function)
+               :initform nil)
    (client-queue :initform (make-array 0 :adjustable t :fill-pointer 0)
                  :accessor client-queue)
    (client-queue-lock :initform (bt:make-lock)
@@ -38,6 +41,10 @@
                      (let ((response (process-message transport message)))
                        (when response
                          (send-message-using-transport transport ws response)))))))
+       (when (slot-value transport 'connect-cb)
+         (wsd:on :open ws
+                 (lambda ()
+                   (funcall (slot-value transport 'connect-cb) ws))))
        (lambda (responder)
          (declare (ignore responder))
          (wsd:start-connection ws))))
@@ -51,11 +58,13 @@
                                          (websocket-transport-host transport)
                                          (websocket-transport-port transport)))))
     (wsd:start-connection client)
+    (setf (transport-connection transport) client)
+    (when (slot-value transport 'connect-cb)
+      (funcall (slot-value transport 'connect-cb) client))
     (wsd:on :message client
             (lambda (message)
               (bt:with-lock-held ((client-queue-lock transport))
                 (vector-push-extend message (client-queue transport)))))
-    (setf (transport-connection transport) client)
     transport))
 
 (defmethod send-message-using-transport ((transport websocket-transport) ws message)
